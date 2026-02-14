aeabafe94b76d19617569b46ebd90a9864495938
Amit Singh <amitsomvanshi63@gmail.com>
2026-02-14 23:09:06 +0530

    palindrome no.

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+# str_1 = "madam"
+# if str_1 == str_1[::-1]:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+str_1 = input("enter the word")
+if str_1 == str_1[::-1]:
+    print("word is palindrome")
+else:
+    print("word is not palindrome")
