dce37808e011a7a2a6772498a2d536e95037efed
Amit Singh <amitsomvanshi63@gmail.com>
2026-02-15 11:00:47 +0530

    variables

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+name = "aliya"
+# city = "pune"
+# print(f"her name is {name}")
+# print(f"she lives in {city}")
---
+++ variables.py
@@ -0,0 +1,35 @@
+age = 23
+# print(age)
+# print("you are " + str(age) + " years old")
+# print ("you are", age, "years old")
+# print(f"you are {age} years old")
+
+# integer
+
+# age = 21
+# distance = 5
+# quantity = 78
+# print(f"your age is {age}")
+# print(f"my house is at a distance of {distance} kms from yours")
+# print(f"we have {quantity} quantities for now")
+
+#float
+
+# price = 100.99
+# weight = 78.5
+# # print(f"the price for this is{price} Rs.")
+# # print (f"her weight is {weight} kgs")
+# print(f"she has bought gym equipments worth {price}$ and her weight is {weight} kgs")
+
+#char
+
+# name = "aliya"
+# city = "pune"
+# print(f"her name is {name}")
+# print(f"she lives in {city}")
+
+#boolean
+online = True
+for_sale = False
+print(f"Are you online?: {online}")
+print(f"is the item for sale?: {for_sale}")
