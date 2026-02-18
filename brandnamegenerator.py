19550fe7d78f7348c81e419cc1a30024f6836d3d
Amit Singh <amitsomvanshi63@gmail.com>
2026-02-18 23:43:50 +0530

    brandnamegenerator

---
+++ brandnamegenerator.py
@@ -0,0 +1,6 @@
+print("welcome to brand name generatot")
+ # take inputs
+city = input("enter your city name: ")
+domain = input("enter your domain name: ")
+brand_name = city + " " + domain
+print(brand_name)
