cdf7b6b7cac9eceb0d2ca7ace6496268c3b28ca0
Amit Singh <amitsomvanshi63@gmail.com>
2026-02-18 23:52:50 +0530

    advanced commit

--- brandnamegenerator.py
+++ brandnamegenerator.py
@@ -1,6 +1,14 @@
-print("welcome to brand name generatot")
- # take inputs
-city = input("enter your city name: ")
-domain = input("enter your domain name: ")
-brand_name = city + " " + domain
+# print("welcome to brand name generatot")
+#  # take inputs
+# city = input("enter your city name: ")
+# domain = input("enter your domain name: ")
+# brand_name = city + " " + domain
+# print(brand_name)
+
+import random
+print("welcome to brand name generator")
+
+names = ["carrer", "hub", "work", "sonnet", "weekday", "workday", "perday", "advanced"]
+domain = ["tech", "sales", "IT", "computer", "my", "data"]
+brand_name = random.choice(names)+ " " + random.choice(domain)
 print(brand_name)
