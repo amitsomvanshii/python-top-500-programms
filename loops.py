4a87a632b9248add76e9e2ce381908bc24a58070
Amit Singh <amitsomvanshi63@gmail.com>
2026-02-23 15:02:46 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,66 @@
+# print hello infinite times
+# while True:
+#     print("hello")
+
+# print finite times hello
+# count = 1
+# while count <= 5:
+#     print("hello", count)
+#     count += 1
+
+# i = 1 
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+#print no.s from 1 to 100
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+#print no.s from 100 to 1
+
+# i = 100
+# while i >=1:
+#     print(i)
+#     i -=1
+
+#multiplication table of number n
+# num_1 = int(input("enter a number"))
+# for i in range(1, 11):
+#     print(num_1* i)
+
+#print the elements of list using loop
+#1,4,9,16,25,36,49,64,81,100
+
+ #loops in list
+# list_1 = ['apple','mango','banana','orange']
+# for x in list_1:
+#     print(x)
+
+#loops in tuple
+# tuple_1 = ('apple','mango','banana','orange')
+# for x in tuple_1:
+#     print(x)
+
+#nested loops
+# for x in range(1, 6):
+#     for y in range(4, 8):
+#         print(x,y)
+
+#control statements in python
+#break, continue, pass
+set_1 = {'apple','mango','banana','oranges'}
+for x in set_1:
+        if x== 'banana':
+            break
+        print(x)
+        
+        
